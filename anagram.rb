6515990ed4d162ae8988457230c7279387bb603c
doberman7 <ivanrubioangeles@gmail.com>
2017-02-19 14:33:52 -0600

    anagram ended

--- anagram.rb
+++ anagram.rb
@@ -7,31 +7,38 @@
 #
 # Una vez que tengas el seudocódigo escribe un método llamado are_anagrams? que reciba dos palabras, sin importar si existen en un diccionario, y devuelva true o false.
 
-def are_anagrams?(word1,word2)
-	word1 = word1.split(//).sort.join
-	word2 = word2.split(//).sort.join
-	word1 == word2 ? true : false;
-end
- p are_anagrams?("sopa","paso")
+# def are_anagrams?(word1,word2)
+# 	word1 = word1.split(//).sort.join
+# 	word2 = word2.split(//).sort.join
+# 	word1 == word2 ? true : false;
+# end
+
 =begin
  Intenta escribir un método llamado canonical que reciba una palabra y la regrese en un formato especifico el cual, si comparas dos anagramas después de mandados a este método sean iguales. No sabes que es canonical visita esta página Forma Canónica
 =end
 def canonical(word)
-	word1 = word1.split(//).sort.join
-	word2 = word2.split(//).sort.join
+	word = word.split(//).sort.join
 end
 
 def are_anagrams?(word1, word2)
   canonical(word1) == canonical(word2)
 end
+
+p  are_anagrams?("sopa","paso")
 =begin
-Ahora intenta crear un método que se llame anagrams_for el cual deberá recibir una palabra y un arreglo de palabras (un diccionario) y regresar un arreglo de las palabras que sean anagramas de esa palabra o un arreglo vació si no encuentra ninguna.
-
+Ahora intenta crear un método que se llame anagrams_for el cual deberá recibir una palabra y un arreglo de palabras (un diccionario)
+y
+regresar un arreglo de las palabras que sean anagramas de esa palabra o un arreglo vació si no encuentra ninguna.
 Recuerda escribir pseudocódigo y crear pruebas como driver code antes de empezar a escribir.
-
 Finalmente refactoriza tu método. Piensa si existe algún proceso que estés repitiendo varías veces y busca una mejor manera de hacerlo, como pista busca como funciona el método select.
 =end
 
 def anagrams_for(word,diccionario)
-
+	result_array = []
+	#CANONICAL method to each element of the array
+	diccionario.select do |i| #COMPARE word with dictinary, use .select? method
+		result_array << i  if canonical(i) == canonical(word)#IF the comparison is truee thenn it is put it inn the result_array
+	end
+	result_array
 end
+p anagrams_for("sopa" , ["sopa","puño","tope","paso"])
