315299fc2e298417e39cc946c6480a047978ea26
doberman7 <ivanrubioangeles@gmail.com>
2017-02-19 16:32:55 -0600

    solved bynary search

--- Algoritmo_Binary_Search.rb
+++ Algoritmo_Binary_Search.rb
@@ -5,27 +5,29 @@ Objetivos Académicos
 Actividades
 Para este ejercicio es importante crees un Driver Code con muchas pruebas para verificar que tu algoritmo es correcto y que siempre regresa lo mismo.
 Para este método es importante que el arreglo este ordenado ascendentemente, tampoco puedes utilizar Array#index y te recomendamos mucho que antes de escribir código escribas pseudocódigo.
-
-DEFINE method binary_search
-  ASIGN number_to_look_for
-  ASIGN array
-
-  BSEARCH number_to_look_for == |i|
-    PUTS   i
 =end
 
-def binary_search(number_to_look_for, array)
-  var = 0
-  array.bsearch do |i|
-    #p i < number_to_look_for
-    p i == number_to_look_for
-    p i
+def binary_search(searching, array)
+half = 0
+  if array.length >= searching
+    last = array.length
+    first = 0
+    while searching != half
+        if searching == 1
+          half = searching
+        elsif  array.length > 0
+          half = ((first + last)/2)
+          first = half if half < searching
+          last = half if half > searching
+        end
+    end
+     half - 1
   end
 end
 
- #binary_search(1, [1]) #== 0
- #binary_search(4, [1,2,3]) #== nil
-# binary_search(4, [1,2,3,4,5]) #== 3
- # binary_search(6, [1,2,3,4,5,6,7,8,9]) #== 5
- # binary_search(2, [1,2,3,5,6,7,8,9,10,11]) #== 1
-  binary_search(5, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]) #== 4
+p binary_search(1, [1]) == 0
+p binary_search(4, [1,2,3]) == nil
+p binary_search(4, [1,2,3,4,5]) == 3
+p binary_search(6, [1,2,3,4,5,6,7,8,9]) == 5
+p binary_search(2, [1,2,3,5,6,7,8,9,10,11]) == 1
+p binary_search(5, [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]) == 4
