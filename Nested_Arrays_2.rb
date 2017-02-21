0ffae3a28c0322e9dc4df436499c8897004362ce
doberman7 <ivanrubioangeles@gmail.com>
2017-02-21 14:30:24 -0600

    nested arrays advace

--- Nested_Arrays_2.rb
+++ Nested_Arrays_2.rb
@@ -9,15 +9,25 @@ X | X | X |
 --  --  --
 =end
 def gato
-convinacion  = "X" , "0" , "O", "O", "X", "O","O", "O", "X"
-	convinacion = Array.new(3) {convinacion.pop(3)}
-	convinacion.each do |line|
-		line.each do |i|  i + "\s|\s"
-		end
-	p convinacion
-		#  print "#{line}\n"
-		#  puts "-------------\n"
+letters = ["X" , "O"]
+9.times do
+ = letters.sample
+ if tiro
 
-	end
+ end
+end
+
+
+convinacion  = "X" , "O" , "O", "O", "X", "O","O", "O", "X"#arreglo con 9 elementos
+result = ""
+	 thre_arrays_of_3 = Array.new(3) {convinacion.pop(3)}#se crea arreglo con tres arreglos con tres elementos cada uno
+	 thre_arrays_of_3.each do |line|#iterar sobre cada arraglo, asignar nombre "line"
+		line.insert(0,"\s|")#insertar: espacio y pipe al principio de "line"
+		line.insert(2,"\s|\s")#insertar: espacio, pipe, espacio  enmedio-1  de "line"
+		line.insert(4,"\s|\s")#insertar: espacio, pipe, espacio  enmedio+1  de "line"
+		line.insert(6,"\s")#insertar: pipe, espiacio  al final de "line"
+		result << line.reverse.join + "\n" + '--  --  --'+"\n"#cada line es INVERTIDO, juntado en un string, se le agrega un \n + linias de tabler + \n
+	 end
+	 result
 end
   gato
