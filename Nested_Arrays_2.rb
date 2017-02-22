a5e14a3b49da6a7c203bc5ca5f84ab2b6b144c58
doberman7 <ivanrubioangeles@gmail.com>
2017-02-22 15:32:40 -0600

    advance in table exercise

--- Nested_Arrays_2.rb
+++ Nested_Arrays_2.rb
@@ -39,23 +39,23 @@ puts gato
 
 table = [ ["Nombre", "Edad", "Genero", "Grupo", "Calificaciones"],
           ["Rodrigo García", "13", "Masculino", "Primero", [9, 9, 7, 6, 8]],
-          ["Fernanda Gonzalez" ,12 ,"Femenino", "Tercero" , [6, 9, 8, 6, 8] ],
+          ["Fernanda Gonzalez" ,"12" ,"Femenino", "Tercero" , [6, 9, 8, 6, 8] ],
           ["Luis Perez", "13" , "Masculino", "Primero", [8, 7, 7, 9, 8] ],
           ["Ana Espinosa","14", "Femenino", "Segundo", [9, 9, 6, 8, 8] ],
           ["Pablo Moran", "11", "Masculino" , "Segundo", [7, 8, 9, 9, 8] ],
 ]
-
-table [0][0] #Nombre
-hash_table = Array.new(1){ Hash.new }
-
-
-#ejemplo: [{"Nombre"=>"Rodrigo García", "Edad"=>13, "Genero"=>"Masculino", "Grupo"=>"Primero", "Calificaciones"=>[9, 9, 7, 6, 8]},...
-
-#compare_by_identity → hsh
-
-#values_at(key, ...) → array
-#Return an array containing the values associated with the given keys. Also see Hash.select.
-
+array = []
+place_to_column = 0
+  for row in 1..5
+      table [row].insert(0, table [0][0])
+      table [row].insert(2, table [0][1])
+      table [row].insert(4, table [0][2])
+      table [row].insert(6, table [0][3])
+      table [row].insert(8, table [0][4])
+  end
+table.delete_at(0)
+p table.flatten(1)
+#EJEMPLO: [{"Nombre"=>"Rodrigo García", "Edad"=>13, "Genero"=>"Masculino", "Grupo"=>"Primero", "Calificaciones"=>[9, 9, 7, 6, 8]},...
 # p table[0][4]  == "Calificaciones"
 # p table[2][1]  == 12
 # p table[2][2]  == "Femenino"
