0d2fc2635c3ee8abc0890b4cb8cc4ad3abd7e4f4
codeamac19 <codeamac19@gmail.com>
2017-02-23 17:19:41 -0600

    fibonachi recursive solved

--- Los_Números_De_Fibonacci.rb
+++ Los_Números_De_Fibonacci.rb
@@ -10,12 +10,16 @@ p fibonacci_iterative(4) == 3
 p fibonacci_iterative(5) == 5
 =end
 def fibonacci_recursive(n)
- 	0 if n == 0
-	1 if n == 1
+  case n
+  when 0 then 0
+  when 1 then 1
+  else
+    fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+  end
 end
-p fibonacci_recursive(0) #== 0
+p fibonacci_recursive(0) == 0
 p fibonacci_recursive(1) == 1
-# p fibonacci_recursive(2) == 1
-# p fibonacci_recursive(3) == 2
-# p fibonacci_recursive(4) == 3
-# p fibonacci_recursive(5) == 5
+p fibonacci_recursive(2) == 1
+p fibonacci_recursive(3) == 2
+p fibonacci_recursive(4) == 3
+p fibonacci_recursive(5) == 5
