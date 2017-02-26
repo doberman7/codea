a980d9027fc46874bca3dca40b98e82e94fecde0
doberman7 <ivanrubioangeles@gmail.com>
2017-02-25 18:21:36 -0600

    edited sopa de letras, intitled bloques

--- Sopa_De_Letras.rb
+++ Sopa_De_Letras.rb
@@ -1,1 +1,2 @@
-p "hello".ljust(20)   #=> "hello123412341234123"
+#.ljust ajusta la cadena de sólo si el argumento es > a string.length
+p "hello".ljust(20, "123")   #=> "hello123412341234123"
--- UntitledBloques_De_Codigo_Blocks.rb
+++ UntitledBloques_De_Codigo_Blocks.rb
@@ -6,22 +6,3 @@
 # greet do
 # 	puts "midle"
 # end
-def canonical(word)
-	word = word.split(//).sort.join
-end
-
-def are_anagrams?(word1, word2)
-  canonical(word1) == canonical(word2)
-end
-
-def anagrams_for
-	result_array = []
-	diccionario.select do |i|
-		result_array << i  if canonical(i) == canonical(word)
-	end
-	result_array
-end
-p anagrams_for do
-	 word = " sopa"
-	 diccionario = ["sopa","puño","tope","paso"]
-end
