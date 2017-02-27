6e046035f5368d99ebb811ce6ddf4c22f764f455
doberman7 <ivanrubioangeles@gmail.com>
2017-02-26 18:00:09 -0600

    UntitledBloques_De_Codigo_Blocks.rb advance

--- UntitledBloques_De_Codigo_Blocks.rb
+++ UntitledBloques_De_Codigo_Blocks.rb
@@ -6,3 +6,16 @@
 # greet do
 # 	puts "midle"
 # end
+p t = Time.now
+def fibonacci_recursive(n)
+  case n
+  when 0 then 0
+  when 1 then 1
+  else
+    fibonacci_recursive(n-1) + fibonacci_recursive(n-2)
+  end
+end
+p fibonacci_recursive(35)
+p t2 = Time.now
+
+p  t2 - t
