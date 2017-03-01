efdbc2aaa00d7af0337d229fe1f1365f0d2a76ec
codeamac19 <codeamac19@gmail.com>
2017-02-28 21:15:04 -0600

    update sopa

--- Sopa_De_Letras.rb
+++ Sopa_De_Letras.rb
@@ -1,7 +1,12 @@
 #.ljust ajusta la cadena de sólo si el argumento es > a string.length     # "hello".ljust(20, "123")   #=> "hello123412341234123"
+#[["POEMA", "CANCION", "RONDAS", "RIMAS"],"POEMAXCXXXXAXXSXNXAAXCMXDXIXXNROXXOXNXXR"]
+#[["MANGO", "SANDIA", "MELON", "PLATANO"],"XXXXPXXXXLXAMXAXIEXTXDLXAXNOXNMANGOXSXXX"]
+#[["ROJO", "VERDE", "AZUL", "NEGRO"],"OJORXXXXXXXEXXOXDXRXXRGLXXEXUXNVXZXXXXAX"]
 class Board
-	@@boards_templates = [["MANGO", "SANDIA", "MELON","PLATANO"],"XXXXPXXXXLXAMXAXIEXTXDLXAXNOXNMANGOXSXXX"]
+	@@boards_templates = [["POEMA", "CANCION", "RONDAS", "RIMAS"],"POEMAXCXXXXAXXSXNXAAXCMXDXIXXNROXXOXNXXR"]
 	@@soup = Array.new(8) { Array.new(0) } # CREATE a multidimensional array of 8 row's
+
+	def made_soup
 		strg = @@boards_templates [1]# ASSGIN the boards_templates[-1] to a variable
 		place_inner_array = 0 # ASSIG a variable for to being use for the next loop
 		init = 0# ASSIGN a variable to  index[0] for tha iner array
@@ -12,7 +17,27 @@ class Board
 			final+=5 # ADD 5 to the index[-1] of inner array
 			place_inner_array += 1 #ADD 1 to the index of the soup
 		end
-		@@soup #RETURN soup
+		@@soup
+	end
+
+	def find
+		word_to_find = @@boards_templates [0][1]#=>CANCION
+		first_letter = word_to_find[0]#C
+		mixed_letters = @@boards_templates [1]#=>"POEMAXCXXXXAXXSXNXAAXCMXDXIXXNROXXOXNXXR"
+		index_fid_wrd = 0
+		until mixed_letters [index_fid_wrd] == first_letter#loop para encontrar primera letra
+			 index_fid_wrd += 1
+		end
+	 	 finded_word = "" << mixed_letters [index_fid_wrd]#ASSIGN first letter of finded in the soup
+		 counter = 0
+		 until counter == word_to_find.length - 1
+		 		finded_word << mixed_letters [index_fid_wrd+=5]
+				counter+=1
+		 end
+		 	p word_to_find == finded_word ? true : false;
+
+	end
+
 
   def initialize
 	 	complete_board!
@@ -31,3 +56,4 @@ end
 
 board = Board.new
 #puts board
+ board.find
