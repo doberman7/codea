ac48b81e43c672e96435f6d49e9655983f2ec968
doberman7 <ivanrubioangeles@gmail.com>
2017-03-25 17:22:44 -0600

    sudoku update, convinacion de grid con fila avance

--- sudoku.rb
+++ sudoku.rb
@@ -7,7 +7,6 @@ class Sudoku
     @copy_original_array = []
   end
 
-
   def solve!
     #Fill in some empty grid with the known values
     @eighty_one_grids[0]
@@ -16,51 +15,33 @@ class Sudoku
       for nx in 0..8
         for ny in 0..8
           value_integer = @value[values_index].to_i #asignar cada valor del string de valores conocidos a una variable local "value_integer"
-          value_integer = nil if value_integer == 0 #SI el valor convertido en entero es "0", significa que esta vacio
+          #value_integer = nil if value_integer == 0 #SI el valor convertido en entero es "0", significa que esta vacio
           @eighty_one_grids[nx][ny] = value_integer #asignamos el value_integer a cada indice nx, ny del arreglo multidimencional
            values_index +=1
         end
       end
 
-      @copy_original_array = @eighty_one_grids #Make an original copy of the array
+    @copy_original_array = @eighty_one_grids #Make an original copy of the array
       #@eighty_one_grids[row][column]
       #@eighty_one_grids[  nx  ][ ny]
       #values => ('702806519100740230005001070008000002610204053200000100070400800064078005821503907'
       #p @eighty_one_grids[  0  ][ 2]
       #Start from top left grid (nx = 0, ny = 0), check if grid is empty
+      row = 0
+      counter = 0
       for nx in 0..8
         for ny in 0..8
-          #if (grid is empty) {
-          if @eighty_one_grids[nx][ny] == nil
-            #    assign the empty grid with values (i)
-            for i in 1..9
-               p @eighty_one_grids
-               p @eighty_one_grids[nx][ny] = i
-               p @eighty_one_grids
-            #    if (no numbers exists in same rows & same columns same as (i) & 3x3 square (i) is currently in)
-            #      fill in the number
-                #  if i != nx && i != ny
-                #   p i
-                #  end
-            #    if (numbers exists in same rows | same columns same as (i) | 3x3 square (i) is currently in)
-            #      discard (i) and repick other values (i++)
-            #  }
-            #  else {
-            #    while (nx < 9) {
-            #      Proceed to next row grid(nx++, ny)
-            #      if (nx equals 9) {
-            #        reset nx = 1
-            #        proceed to next column grid(nx,ny++)
-            #        if (ny equals 9) {
-            #          print solution
-            #        }
-            #      }
-            #    }
-            #  }
-            end
+          counter +=1
+          p @eighty_one_grids[nx][ny]
+          p @eighty_one_grids[row]
+          if counter % 9 == 0
+            p "---------------------------"
+            @eighty_one_grids[row]
+            row += 1
           end
         end
       end
+      #p @eighty_one_grids
   end
 
   def board
